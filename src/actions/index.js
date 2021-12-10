144bec1fe7d53349a21202107310f79f885c994a
kiran <kletherwala@gmail.com>
2021-12-09 19:52:30 -0600

    initial commit added libraries

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,43 @@
 import axios from 'axios';
 
+// export const fetchSmurfs = () => {
+// return (dispatch) => {
+// dispatch(fetchStart())
+
+// axios.get('http://localhost:3333/smurfs')
+//     .then(res => {
+//        dispatch(fetchSuccess(res.data))
+//     })
+//     .catch(err => {
+//         dispatch(fetchError(err))
+//     })
+// }
+// }
+
+// export const FETCH_START = 'FETCH_START'
+
+// export const fetchStart = () => {
+//     return ({ type: FETCH_START })
+// }
+
+// export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+
+// export const fetchSuccess = (smurf) => {
+//     return ({ type: FETCH_SUCCESS, payload: smurf })
+// }
+
+// export const ADD_SMURF = 'ADD_SMURF'
+
+// export const addSmurf = (smurf) => {
+//     return ({ type: ADD_SMURF, payload: smurf })
+// }
+
+// export const FETCH_ERROR = 'FETCH_ERROR'
+
+// export const fetchError = (errorMessage) => {
+//     return ({ type: FETCH_ERROR, payload: errorMessage })
+// }
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
